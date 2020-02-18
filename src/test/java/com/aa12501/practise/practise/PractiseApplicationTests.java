33e067a68adc8b6944c79c50d9b6c03c6eec44ad
aa12501 <517052851@qq.com>
2020-02-19 00:37:32 +0800

    my first step

---
+++ src/test/java/com/aa12501/practise/practise/PractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aa12501.practise.practise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
